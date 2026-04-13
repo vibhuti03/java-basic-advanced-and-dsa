acc6ab23d14b7f6da8a4f4ddedfafa5512c15e71
vibhuti03 <vibhutimaheshwari03@gmail.com>
2026-04-13 15:52:29 +0530

    Recursion2

---
+++ src/Recursion/PalindromicStringEasy.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class PalindromicStringEasy {
+    static boolean checkPalindrome(int i, String s){
+        int n = s.length();
+        if(i >= n/2){
+            return true;
+        }
+
+        if (s.charAt(i) != s.charAt(n-i-1)){
+            return false;
+        }
+        return checkPalindrome(i+1, s);
+    }
+
+    public static void main(String[] args) {
+        String s = "madam";
+        System.out.println(checkPalindrome(0, s));
+    }
+}
--- src/Recursion/SwapArrayEasy.java
+++ src/Recursion/SwapArrayEasy.java
@@ -2,7 +2,7 @@ package Recursion;
 
 import java.util.Arrays;
 
-public class SwapArray {
+public class SwapArrayEasy {
     static void swap(int i, int j, int[] arr){
         int temp = arr[i];
         arr[i] = arr[j];
@@ -21,7 +21,7 @@ public class SwapArray {
     static void swapArraySinglePointer(int i, int[] arr){
         int n = arr.length;
 
-        if(i==(n/2)){
+        if(i>=(n/2)){
             System.out.println(Arrays.toString(arr));
             return;
         }
@@ -30,7 +30,7 @@ public class SwapArray {
 
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,2,3,4,5,6};
 
         int left = 0, right = arr.length-1;
         swapArrayTwoPointer(left, right, arr); // => [6,5,4,3,2,1]
